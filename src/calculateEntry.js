e0126b541305bcd145ce5d1e89abfc6581ad05c9
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-05-05 20:11:49 -0300

    começouuu

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -5,7 +5,7 @@ const countEntrants = (entrants) => {
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  // seu código aquia
 };
 
 module.exports = { calculateEntry, countEntrants };
