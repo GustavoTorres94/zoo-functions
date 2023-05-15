c43318b2aa2e74d886892931e0bab4f234c9a8ea
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-05-15 19:33:30 -0300

    alguns feitos2

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const verifySex = (animal) => {
+  const bicho = species.find((e) => e.name === animal.species);
+  const macho = bicho.residents.filter((e) => e.sex === 'male');
+  const femea = bicho.residents.filter((e) => e.sex === 'female');
+  return animal.sex === macho ? macho.length : femea.length;
+};
 
 const countAnimals = (animal) => {
-// code
+  if (!animal) {
+    return species.reduce((acc, curr) => ({ ...acc, [curr.name]: curr.residents.length }), {});
+  }
+  if (animal.species && animal.sex) {
+    return verifySex(animal);
+  }
+  if (animal.species) {
+    const bicho = species.find((e) => e.name === animal.species);
+    return bicho.residents.length;
+  }
 };
-countAnimals();
+
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,10 +1,21 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // code
-};
+const isManager = (id) => data.employees.some((e) => e.managers.includes(id));
+
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  try {
+    const employees = data.employees
+      .filter((e) => e.managers.includes(managerId))
+      .map((e) => `${e.firstName} ${e.lastName}`);
+    return employees;
+  } catch (err) {
+    return err.message;
+  }
 };
+
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
